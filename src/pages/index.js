3c2329ce338e0ff0727d40765e11fcf3636edf4c
yukyu30 <fukanoyugo+dev@gmail.com>
2022-01-02 15:55:54 +0900

    リンクを修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="docs/term-of-use">
             規約を確認する
           </Link>
         </div>
