82b9c3fe79db39d3029e0a0aae0d48c9255f34f6
yu5 <fukanoyugo+dev@gmail.com>
2023-08-25 13:44:47 +0900

    TOPページのメンテナンスができていないので削除する

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -3,39 +3,31 @@ import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
-//   {
-//     title: 配布先,
-//     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-//     description: (
-//       <>
-//         Docusaurus was designed from the ground up to be easily installed and
-//         used to get your website up and running quickly.
-//       </>
-//     ),
-//   },
-  {
-    title: (
-        <>
-          <a href='https://uq-konpeki-dou.booth.pm/'>Booth</a>
-        </>
-      ),
-    Svg: require('../../static/img/download.svg').default,
-    description: (
-      <>
-        こちらから配布物をダウンロードできます。
-      </>
-    ),
-  },
-//   {
-//     title: 'Powered by React',
-//     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-//     description: (
-//       <>
-//         Extend or customize your website layout by reusing React. Docusaurus can
-//         be extended while reusing the same header and footer.
-//       </>
-//     ),
-//   },
+  //   {
+  //     title: 配布先,
+  //     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+  //     description: (
+  //       <>
+  //         Docusaurus was designed from the ground up to be easily installed and
+  //         used to get your website up and running quickly.
+  //       </>
+  //     ),
+  //   },
+  // {
+  //   title: 'yukyu30が制作したものに関する利用規約がまとまっているページです',
+  //   Svg: require('../../static/img/download.svg').default,
+  //   description: <>こちらから配布物をダウンロードできます。</>,
+  // },
+  //   {
+  //     title: 'Powered by React',
+  //     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+  //     description: (
+  //       <>
+  //         Extend or customize your website layout by reusing React. Docusaurus can
+  //         be extended while reusing the same header and footer.
+  //       </>
+  //     ),
+  //   },
 ];
 
 function Feature({Svg, title, description}) {
